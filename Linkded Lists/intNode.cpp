20b26b51abbf42424a00b7322220c24a9e63d7e4
Kuldeep <codysharma01@gmail.com>
2021-04-29 15:16:20 +0530

    added operations on Linked Lists

--- Linkded Lists/intNode.cpp
+++ Linkded Lists/intNode.cpp
@@ -11,7 +11,7 @@ class Node{
     :data(data), next(nullptr){}
 };
 
-// pirinting node func
+// printing node func
 void print(const Node *head){
     while (head != nullptr){
         std::cout << head->data << ' ';
@@ -20,6 +20,7 @@ void print(const Node *head){
     std::cout << std::endl;
 }
 
+// Creates new linked list 
 Node* createLinkedList(void){
     // takes space seperated input untill newLine character is appeared
         // lil bug found: waits for  new value if existing input isn't stripped
@@ -51,4 +52,133 @@ Node* createLinkedList(void){
         head = new Node(data);
         
     return head;
+}
+
+// returns length of the list
+int getLength(Node* head){
+// recursive approach
+    // base case
+    if(head == nullptr)
+        return 0;
+    
+    //recursive call
+    int smallOutput = getLength(head->next);
+    //smaller Calculation
+    return smallOutput+1;
+
+// iterative approch
+    // int len = 0;
+    // while(head!=nullptr){
+    //     head = head->next;
+    //     len++;
+    // }
+    // return len;
+}
+
+// print i-th position 
+void printIthPosition(Node* head, int i){
+    int lengthOfList = getLength(head);
+    if(i > lengthOfList)
+        return;
+    
+    // Node* temp = head;
+    for(int j = 0; j<i; j++){
+        head = head->next;
+    }std::cout << head->data << std::endl;
+}
+
+// insert at i-th position
+Node *insertIthNode(Node* head, int data, int i){
+// recursive approach
+    // special case:
+    if(head == nullptr){
+        if(i!=0)
+            return head;
+        //add element at ending
+        return new Node(data);
+    }
+
+    // smaller Calculation and recursive call
+    if(i==0){
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }else{
+        head->next = insertIthNode(head->next, data, i - 1);
+        return head;
+    }
+
+// iterative approach
+    // Node *newNode = new Node(data);
+    // // if we we to add at first position
+    // if(i==0){
+    //     newNode->next = head;
+    //     head = newNode;
+    //     return head;
+    // }
+    // // insert at any other position
+    // int count = 0;
+    // // reach at tail-1 position for insertion
+    // Node* temp = head;
+    // while(count < i-1 && temp != nullptr){
+    //     temp = temp->next;
+    //     count++;
+    // }
+    // // if position to insert is beyond maximium legth exit
+    // if(temp == nullptr)
+    //     return head;
+
+    // // insertion
+    // newNode->next = temp->next;
+    // temp->next = newNode;
+
+    // return head;
+
+}
+
+// delete i-th node
+Node* deleteIthNode(Node* head, int i){
+// recursicve approach
+    //base case
+    if(head == nullptr)
+        return head;
+    
+    //smaller calculation
+    if(i==0){
+        Node *nodeToBeDeleted = head;
+        head = head->next;
+        delete nodeToBeDeleted;
+        return head;
+    }
+
+    // recursive call
+    head->next = deleteIthNode(head->next,i-1);
+    return head;
+
+// iterative approach
+    // // if nodeToBe deleted is first
+    // if(i==0){
+    //     Node *nodeToBeDeleted = head;
+    //     head = head->next;
+    //     delete nodeToBeDeleted;
+    //     return head;
+    // }
+    // // getting tail-i position
+    // int count = 0;
+    // Node* temp = head;
+    // while(count < i-1 && temp != nullptr){
+    //     temp = temp->next;
+    //     count++;
+    // }
+    // // when we reached beyond length of list exit
+    // if(temp == nullptr)
+    //     return head;
+    
+    // // deletation
+    // Node* nodeToBeDeleted = temp->next;
+    // Node* nextNode = nodeToBeDeleted->next;
+    // temp->next = nextNode;
+    // delete nodeToBeDeleted;
+    // return head;
 }
--- Linkded Lists/intNodeMain.cpp
+++ Linkded Lists/intNodeMain.cpp
@@ -4,5 +4,15 @@ int main(void)
 {
     Node *head = createLinkedList();
     print(head);
+
+    int data, i;
+    std::cin >>data>> i;
+    head = insertIthNode(head,data,i);
+    // head = deleteIthNode(head,i);
+    print(head);
+    head = deleteIthNode(head, i);
+    print(head);
+    std::cout << getLength(head) << std::endl;
+
     return 0;
 }
