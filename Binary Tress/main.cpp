1446ad677368157c0c1bce85b530af2915e74b30
Kuldeep <codysharma01@gmail.com>
2021-06-09 11:19:07 +0530

    added few functions

--- Binary Tress/main.cpp
+++ Binary Tress/main.cpp
@@ -7,7 +7,6 @@ template <class T> void printTree(BinaryTreeNode<T> *root) {
   // checck if root isn't null
   if (root == nullptr)
     return;
-
   // create queue
   std::queue<BinaryTreeNode<T> *> pendingNodes;
   pendingNodes.push(root);
@@ -102,8 +101,7 @@ template <class T> int getHeight(BinaryTreeNode<T> *root) {
   int leftChildLevel = getHeight(root->left);
   int rightChildLevel = getHeight(root->right);
 
-  return (leftChildLevel > rightChildLevel) ? 1 + leftChildLevel
-                                            : 1 + rightChildLevel;
+  return 1 + std::max(leftChildLevel, rightChildLevel);
 }
 
 // mirror nodes of a tree
@@ -148,6 +146,60 @@ template <class T> void postorderPrint(BinaryTreeNode<T> *root) {
   std::cout << root->data << ' ';
 }
 
+// get height and diameter of tree
+template <class T>
+std::pair<int, int> getHeightDiamter(BinaryTreeNode<T> *root) {
+  // using pair's first for height and second for diameter
+  if (root == nullptr) {
+    std::pair<int, int> p;
+    p.first = 0;
+    p.second = 0;
+
+    return p;
+  }
+
+  std::pair<int, int> leftSideData = getHeightDiamter(root->left);
+  std::pair<int, int> rightSideData = getHeightDiamter(root->right);
+
+  int lh = leftSideData.first;
+  int rh = rightSideData.first;
+  int ld = leftSideData.second;
+  int rd = rightSideData.second;
+
+  std::pair<int, int> ansPair;
+  ansPair.first = 1 + std::max(lh, rh);
+  ansPair.second = std::max(lh + rh, std::max(ld, rd));
+
+  return ansPair;
+}
+
+template <class T> std::pair<int, int> getMinMax(BinaryTreeNode<T> *root){
+  // using pair's first for minimum and second for maximum
+  // edge case and base case
+  if (root == nullptr) {
+    std::pair<int, int> p;
+    p.first = INT16_MAX;
+    p.second = INT16_MIN;
+
+    return p;
+  }
+
+  std::pair<int, int> leftSideData = getMinMax(root->left);
+  std::pair<int, int> rightSideData = getMinMax(root->right);
+
+  int lmin = leftSideData.first;
+  int rmin = rightSideData.first;
+  int lmax = leftSideData.second;
+  int rmax = rightSideData.second;
+
+  std::pair<int, int> ans;
+  ans.first = std::min(root->data, std::min(lmin, rmin));
+  ans.second = std::max(root->data,std::max(lmax, rmax));
+
+  return ans;
+
+}
+
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1
 int main(void) {
   BinaryTreeNode<int> *root = takeInput<int>();
@@ -159,12 +211,19 @@ int main(void) {
   // std::cout << std::boolalpha << isPresent(root,x) << std::endl;
   // std::cout << countNodes(root) << std::endl;
   // std::cout << getHeight(root) << std::endl;
-  inorderPrint(root);
-  std::cout << std::endl;
-  preorderPrint(root);
-  std::cout << std::endl;
-  postorderPrint(root);
-  std::cout << std::endl;
+  // inorderPrint(root);
+  // std::cout << std::endl;
+  // preorderPrint(root);
+  // std::cout << std::endl;
+  // postorderPrint(root);
+  // std::cout << std::endl;
+  std::pair<int, int> ans = getHeightDiamter(root);
+  std::cout << "Height: " << ans.first << std::endl;
+  std::cout << "Diameter: " << ans.second << std::endl;
+
+  std::pair<int, int> minmax = getMinMax(root);
+  std::cout << "Min: " << minmax.first << std::endl;
+  std::cout << "Max: " << minmax.second << std::endl;
 
   delete root;
   return 0;
