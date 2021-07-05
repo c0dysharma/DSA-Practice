871c087a1279b7b3d6f6b669f1036cc2575f52d2
Kuldeep <codysharma01@gmail.com>
2021-07-05 15:15:31 +0530

    few ops

--- Hashmaps/main.cpp
+++ Hashmaps/main.cpp
@@ -76,6 +76,106 @@ int returnPairSum(int *arr, int size) {
   return pair;
 }
 
+// return distinct character string
+std::string returnDistanctCharacters(std::string str) {
+  // creating map for storing freq
+  std::unordered_map<char, int> mp;
+  std::string newStr = "";
+
+  for (char a : str) {
+    // its already there skip the character
+    if (mp.count(a) > 0)
+      continue;
+    // its not here add to newStr and increase frequency
+    mp[a]++;
+    newStr += a;
+  }
+
+  return newStr;
+}
+
+// void print longest sequence range [x y]
+void printLongestSequence(int *arr, int size) {
+  // create a map for all values as key and bool as value
+  // with False if we haven't considered for sequence True if we did
+  std::unordered_map<int, bool> mp;
+  for (int i = 0; i < size; i++) {
+    mp[arr[i]] = false;
+  }
+
+  // now starting from first value we will seek sequence forward and backword
+  int maxLength = 1;
+  int start = arr[0];
+  int end = arr[0];
+
+  for (int i = 0; i < size; i++) {
+    // if we already considered skip
+    if (mp[arr[i]] == true)
+      continue;
+
+    mp[arr[i]] = true;
+    int currLength = 1;
+
+    // move forword till we don't find end of sequence
+    int currEnd = arr[i] + 1;
+    while (mp.count(currEnd) > 0) {
+      mp[currEnd] = true;
+      currLength++;
+      currEnd++;
+    }
+    // move backword as well
+    int currStart = arr[i] - 1;
+    while (mp.count(currStart) > 0) {
+      mp[currStart] = true;
+      currLength++;
+      currStart--;
+    }
+    // incrementing start coz we reduced 1 further
+    currStart++;
+    // same for the end
+    currEnd--;
+
+    // update the value of final vars if we gets maximum length
+    if (currLength > maxLength) {
+      maxLength = currLength;
+      start = currStart;
+      end = currEnd;
+    }
+    // totally optional
+    // also update if lenghth is same iff this range starts with curr value
+    if (currLength == maxLength && currStart == arr[i]) {
+      start = currStart;
+      end = currEnd;
+    }
+  }
+
+  std::cout << start << " " << end << std::endl;
+}
+
+// return # of pair difference to k
+int returnDiffrenceKPair(int *arr, int size, int K) {
+  // creating map for O(1) access later
+  std::unordered_map<int, int> mp;
+  for (int i = 0; i < size; i++) {
+    mp[arr[i]]++;
+  }
+  // find possible pair and increment pair counts
+  int pairCount = 0;
+  for (int i = 0; i < size; i++) {
+    int currVal = mp[arr[i]];
+    if(currVal == 0)
+      continue;
+
+    pairCount += currVal * mp[arr[i] + K];
+    mp[arr[i] + K] = 0;
+    pairCount += currVal * mp[arr[i] - K];
+    mp[arr[i] - K] = 0;
+
+    mp[arr[i]] = 0;   
+  }
+  return pairCount;
+}
+
 int main(void) {
   //   int arr[11] = {1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 8};
   //   std::vector<int> items = removeDuplicates(arr, 11);
@@ -90,8 +190,25 @@ int main(void) {
   // printIntersection(arr1, 6, arr2, 4);
   // std::cout << std::endl;
 
-  int sum0[4] = {2, 1, -2, 3};
-  std::cout << returnPairSum(sum0, 4) << std::endl;
+  // int sum0[4] = {2, 1, -2, 3};
+  // std::cout << returnPairSum(sum0, 4) << std::endl;
 
+  // std::string str;
+  // std::cin >> str;
+
+  // str = returnDistanctCharacters(str);
+  // std::cout << "New string: " << str << std::endl;
+
+  int len;
+  std::cin >> len;
+  int arr[len];
+  for (int i = 0; i < len; i++) {
+    std::cin >> arr[i];
+  }
+  int K;
+  std::cin >> K;
+
+  // printLongestSequence(arr, len);
+  std::cout << returnDiffrenceKPair(arr, len, K) << std::endl;
   return 0;
 }
