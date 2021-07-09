d7872d1f7fc2cade6a56ec59690973ce66bcb2a8
Kuldeep <codysharma01@gmail.com>
2021-07-09 13:49:29 +0530

    added priority queues of max elements

--- Priority Queues/pq.h
+++ Priority Queues/pq.h
@@ -1,7 +1,7 @@
 #include <vector>
 
 class PriorityQueue {
-  private:
+private:
   std::vector<int> queue;
 
 public:
@@ -55,6 +55,70 @@ public:
       std::swap(queue[parentIndex], queue[minIndex]);
 
       parentIndex = minIndex;
+      childIndex1 = 2 * parentIndex + 1;
+      childIndex2 = 2 * parentIndex + 2;
+    }
+
+    return ans;
+  }
+};
+
+// Same class but giving priotity to larger number
+class PriorityQueueMax {
+private:
+  std::vector<int> queue;
+
+public:
+  int getSize() { return queue.size(); }
+  bool isEmpty() { return queue.size() == 0; }
+  int getMax() { return (isEmpty()) ? 0 : queue.at(0); }
+
+  void insert(int element) {
+    // add the element to the end
+    queue.push_back(element);
+    // tranverse till we reach top
+    int childIndex = queue.size() - 1;
+    while (childIndex > 0) {
+      int parentIndex = (childIndex - 1) / 2;
+
+      if (queue[childIndex] > queue[parentIndex]) // we need to swap
+        std::swap(queue[childIndex], queue[parentIndex]);
+      else // we have to break its alredy there
+        break;
+
+      // after swaping now
+      childIndex = parentIndex;
+    }
+  }
+
+  int removeMin() {
+    // get the min value before removing
+    int ans = queue[0];
+    queue[0] = queue.back();
+    queue.pop_back();
+
+    // traverse till we get to last node
+    int parentIndex = 0;
+    int childIndex1 = 2 * parentIndex + 1;
+    int childIndex2 = 2 * parentIndex + 2;
+    while (childIndex1 < queue.size()) {
+
+      // get the correct minimum Index
+      int maxIndex = parentIndex;
+      if (queue[childIndex1] > queue[maxIndex])
+        maxIndex = childIndex1;
+
+      if (queue[childIndex2] > queue[maxIndex] && childIndex2 < queue.size())
+        maxIndex = childIndex2;
+
+      // if parent index is smallest we have nothing to do
+      if (maxIndex == parentIndex)
+        break;
+
+      // swap otherwise
+      std::swap(queue[parentIndex], queue[maxIndex]);
+
+      parentIndex = maxIndex;
       childIndex1 = 2 * parentIndex + 1;
       childIndex2 = 2 * parentIndex + 2;
     }
